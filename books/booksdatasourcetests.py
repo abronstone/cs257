0e956c4994adfad70278101f51f860df2bc0fcbc
abronstone <bronstonea@carleton.edu>
2022-09-22 20:06:04 -0500

    9/22/2022: adding books.csv and first draft of booksdatasourcetester.py

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -18,6 +18,38 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(len(authors) == 1)
         self.assertTrue(authors[0] == Author('Pratchett', 'Terry'))
 
+    def test_length(self):
+        books = self.booksdatasource.books('The')
+        self.assertTrue(len(books)<=len(self.data_source))
+
+    def test_book_titles(self, name, order):
+        books = self.data_source.books(name)
+        for b in books:
+            self.assertTrue(b.title.find(name)!=-1)
+        if order in ['-t','--title','']:
+            self.assertEqual(books.sort(key=title),books)
+        if order in ['-y','--year']:
+            self.assertEqual(books.sort(key=publication_year),books)
+        self.assertEqual(books(name),books(name.upper()))
+            
+            
+    def test_authors(self, name):
+        authors = self.booksdatasource.authors(name)
+        for b in books:
+            self.assertTrue(b.author.find(name)!=-1)
+        self.assertEqual(authors(name),authors(name.upper()))
+        self.assertEqual(authors(name),authors(name.lower()))
+        self.assertEqual(authors.sort(key=name.substring(find(' '))),authors)
+
+
+    def test_years(self, start, end):
+        self.assertTrue(start<=end)
+        years = self.booksdatasource.books_between_years('1930','1940')
+        self.assertEqual(years, [And_Then_There_Were_None,Murder_on_the_Orient_Express,The_Code_of_the_Woosters])
+        self.assertEqual(years.sort(key=publication_year),years)
+
+        
+
 if __name__ == '__main__':
     unittest.main()
 
