18859e9881c520067fdf3f31983b0fc0d8fac599
Jack Owens <jackowens@MacBook-Pro-22.local>
2022-11-02 09:15:11 -0500

    Pushed webapp again because git push didn't go through on Saturday 10/29

---
+++ webapp/webapp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/main')
+def get_help():
+    return 'This is the main page, no html file found here!'
+
+@app.route('/')
+def html():
+    return render_template('cheat_sheet.html')
+
+if __name__=='__main__':
+    host = 'localhost'
+    port = 5000
+    app.run(host=host,port=port,debug=True)
