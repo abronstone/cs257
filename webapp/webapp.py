9ddb9e0e9eed547a92416d7d7d42d88d3dc15273
abronstone <bronstonea@carleton.edu>
2022-11-03 16:05:31 -0500

    Added index.html

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -11,8 +11,24 @@ def get_help():
     return 'This is the main page, no html file found here!'
 
 @app.route('/')
-def html():
-    return render_template('cheat_sheet.html')
+def main_page():
+    return render_template('mockup1.html')
+
+@app.route('/search')
+def search_page():
+    return render_template('mockup2.html')
+
+@app.route('/overviews')
+def overview_page():
+    return render_template('mockup3.html')
+
+@app.route('/popularity')
+def popularity_page():
+    return render_template('mockup4.html')
+
+@app.route('/comparison')
+def comparison_page():
+    return render_template('mockup5.html')
 
 if __name__=='__main__':
     host = 'localhost'
